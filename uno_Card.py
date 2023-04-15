045160a7098928f7d563a82be04ee0382f72487c
wedongguk <f4wedk@gmail.com>
2023-04-15 23:34:14 +0900

    dd

--- uno_Card.py
+++ uno_Card.py
@@ -36,26 +36,7 @@ class Card(pygame.sprite.Sprite): # 카드 클래스 생성
             return False
 
     def cardEffect(self, game): # 특수 카드의 효과를 처리하기 위한 메서드.
-        eCode = self.effectCode
-        if eCode & EFFECT_DRAW == EFFECT_DRAW: # 다음 상대에게 카드를 주는 효과
-            game.playerList[(game.turnPlayer+1)%len(game.playerList)].draw(game, self.attackNumber)
-        
-        if eCode & EFFECT_SKIP == EFFECT_SKIP: # 다음 플레이어를 스킵하는 효과
-            self.jumpNumber += 1
-
-        if eCode & EFFECT_REVERSE == EFFECT_REVERSE: # 턴의 진행 방향을 반대로 바꾸는 효과
-            tp = game.turnPlayer
-            tmp_Lst = []
-
-            for _ in range(len(game.playerList)):
-                tp -= 1
-                tmp_Lst.append(game.playerList[tp])
-
-        if eCode & EFFECT_COLOR == EFFECT_COLOR: # 카드의 색을 바꾸는 효과
-            applyColor = int(input())
-            
-        if eCode & EFFECT_NUMBER == EFFECT_NUMBER: # 카드의 숫자를 바꾸는 효과
-            applyColor = int(input())
+        effect(self, game)
         
     def canUse(self, game):
         top = game.openCard.cardList[-1]
