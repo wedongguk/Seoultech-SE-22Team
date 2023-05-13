9c640da72d8f541afff5a3a0085bbfa34879ba26
SeungGu Kang <alex9366@naver.com>
2023-05-13 18:24:27 +0900

    해상도 문제 해결

--- Data/GAME_VIEW/SCREEN/story_mode.py
+++ Data/GAME_VIEW/SCREEN/story_mode.py
@@ -1,7 +1,5 @@
 from Data.GAME_LOGIC.uno_Const import MODE_NORMAL, MODE_ALLCARD, MODE_CHANGECOLOR, MODE_OPENSHUFFLE
 from Data.GAME_VIEW.OBJECT.button import Button
-from Data.GAME_VIEW.OBJECT.text import Text
-from Data.GAME_VIEW.OBJECT.textbox import TextBox
 from Data.GAME_VIEW.OBJECT.view import init_view
 from Data.GAME_VIEW.util import *
 
@@ -20,24 +18,29 @@ def story_mode(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
     y_pos = SCREEN_HEIGHT / 2 - BUTTON_HEIGHT / 2
 
     back_button = Button(image=pygame.image.load(BUTTON_PATH + "back_button.png"),
-                         pos=(30, 30),
-                         size=(50, 50))
+                         pos=(set_size(30, SCREEN_WIDTH), set_size(30, SCREEN_WIDTH)),
+                         size=(set_size(50, SCREEN_WIDTH), set_size(50, SCREEN_WIDTH)))
 
     story_mode_1 = Button(image=pygame.image.load(BUTTON_PATH + "story_mode_1.png"),
-                          pos=(SCREEN.get_rect().centerx - 420, SCREEN.get_rect().centery - 200),
-                          size=(70, 70))
+                          pos=(SCREEN.get_rect().centerx - set_size(420, SCREEN_WIDTH),
+                               SCREEN.get_rect().centery - set_size(200, SCREEN_WIDTH)),
+                          size=(set_size(70, SCREEN_WIDTH), set_size(70, SCREEN_WIDTH)))
 
     story_mode_2 = Button(image=pygame.image.load(BUTTON_PATH + "story_mode_2.png"),
-                          pos=(SCREEN.get_rect().centerx - 400, SCREEN.get_rect().centery + 100),
-                          size=(70, 70))
+                          pos=(SCREEN.get_rect().centerx - set_size(400, SCREEN_WIDTH),
+                               SCREEN.get_rect().centery + set_size(100, SCREEN_WIDTH)),
+                          size=(set_size(70, SCREEN_WIDTH), set_size(70, SCREEN_WIDTH)))
 
     story_mode_3 = Button(image=pygame.image.load(BUTTON_PATH + "story_mode_3.png"),
-                          pos=(SCREEN.get_rect().centerx + 150, SCREEN.get_rect().centery - 180),
-                          size=(70, 70))
+                          pos=(SCREEN.get_rect().centerx + set_size(150, SCREEN_WIDTH),
+                               SCREEN.get_rect().centery - set_size(180, SCREEN_WIDTH)),
+                          size=(set_size(70, SCREEN_WIDTH), set_size(70, SCREEN_WIDTH)))
 
     story_mode_4 = Button(image=pygame.image.load(BUTTON_PATH + "story_mode_4.png"),
-                          pos=(SCREEN.get_rect().centerx + 390, SCREEN.get_rect().centery + 110),
-                          size=(70, 70))
+                          pos=(SCREEN.get_rect().centerx + set_size(390, SCREEN_WIDTH),
+                               SCREEN.get_rect().centery + set_size(110, SCREEN_WIDTH)),
+                          size=(set_size(70, SCREEN_WIDTH), set_size(70, SCREEN_WIDTH)))
+
     start_button = Button(image=pygame.image.load(BUTTON_PATH + "start_button.png"),
                           pos=(x_pos, y_pos + set_size(260, SCREEN_WIDTH)),
                           size=(BUTTON_WIDTH, BUTTON_HEIGHT))
@@ -46,7 +49,7 @@ def story_mode(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
 
     def show_popup(stage):
         global text, mode
-        font = pygame.font.Font(None, 36)
+        font = pygame.font.Font(None, set_size(36, SCREEN_WIDTH))
         description = [
             "첫 분배시 컴퓨터 플레이어가 기술 카드를 50% 더 높은 확률로 받게 됨.컴퓨터 플레이어가 거꾸로 진행과 건너 뛰기 등의 기술카드를 적절히 조합하여 2~3장 이상의 카드를 한 번에 낼 수 있는 콤보를 사용.",
             "3명의 컴퓨터 플레이어와 대전 / 첫 카드를 제외하고 모든 카드를 같은 수만큼 플레이어들에게 분배.",
@@ -64,10 +67,22 @@ def story_mode(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
         init_bg(SCREEN, SCREEN_PATH + "story_mode_map.png", SCREEN_WIDTH, SCREEN_HEIGHT)
         init_view(SCREEN, [back_button, story_mode_1, story_mode_2, story_mode_3, story_mode_4, start_button])
 
+        close_button = Button(image=pygame.image.load(BUTTON_PATH + "back_button.png"),
+                         pos=(SCREEN_WIDTH - text_rect.left - set_size(10, SCREEN_WIDTH) - 10,
+                              text_rect.top - set_size(10, SCREEN_WIDTH) + set_size(200, SCREEN_WIDTH)),
+                         size=(30, 30))
+
         pygame.draw.rect(SCREEN, (255, 255, 255),
-                         (text_rect.left - 10, text_rect.top - 10, text_rect.width + 20, text_rect.height + 20))
+                         (text_rect.left - set_size(10, SCREEN_WIDTH),
+                          text_rect.top - set_size(10, SCREEN_WIDTH),
+                          text_rect.width + set_size(20, SCREEN_WIDTH),
+                          text_rect.height + set_size(20, SCREEN_WIDTH)))
+
         pygame.draw.rect(SCREEN, (0, 0, 0),
-                         (text_rect.left - 10, text_rect.top - 10, text_rect.width + 20, text_rect.height + 20), 2)
+                         (text_rect.left - set_size(10, SCREEN_WIDTH),
+                          text_rect.top - set_size(10, SCREEN_WIDTH),
+                          text_rect.width + set_size(20, SCREEN_WIDTH),
+                          text_rect.height + set_size(20, SCREEN_WIDTH)), 2)
 
         SCREEN.blit(text, text_rect)
         pygame.display.flip()
@@ -84,21 +99,12 @@ def story_mode(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
                     mode = "A"
                     show_popup(mode)
                     from Data.GAME_VIEW.SCREEN.loby import loby
-                    # loby(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
                 elif story_mode_2.rect.collidepoint(event.pos):
                     mode = "B"
                     show_popup(mode)
-                    # from Data.GAME_VIEW.SCREEN.start import start_game
-                    # start_game(int(config['system']['SCREEN_WIDTH']), int(config['system']['SCREEN_HEIGHT']), 3,
-                    #            ["USER", "COMPUTER1", "COMPUTER2", "COMPUTER3", "COMPUTER4"], color_weakness_value,
-                    #            MODE_ALLCARD)
                 elif story_mode_3.rect.collidepoint(event.pos):
                     mode = "C"
                     show_popup("C")
-                    # from Data.GAME_VIEW.SCREEN.start import start_game
-                    # start_game(int(config['system']['SCREEN_WIDTH']), int(config['system']['SCREEN_HEIGHT']), 2,
-                    #            ["USER", "COMPUTER1", "COMPUTER2"], color_weakness_value,
-                    #            MODE_CHANGECOLOR)
                 elif story_mode_4.rect.collidepoint(event.pos):
                     from Data.GAME_VIEW.SCREEN.loby import loby
                     loby(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT, mode=MODE_OPENSHUFFLE)
