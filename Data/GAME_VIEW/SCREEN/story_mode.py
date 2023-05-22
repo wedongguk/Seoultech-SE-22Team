3d1805fec2f65a876f0228d0c5434fe9ca5a919e
SeungGu Kang <alex9366@naver.com>
2023-05-23 02:50:55 +0900

    스토리모드 D 팝업 추가

--- Data/GAME_VIEW/SCREEN/story_mode.py
+++ Data/GAME_VIEW/SCREEN/story_mode.py
@@ -1,4 +1,5 @@
-from Data.GAME_LOGIC.uno_Const import MODE_NORMAL, MODE_ALLCARD, MODE_CHANGECOLOR, MODE_OPENSHUFFLE
+import pygame.image
+from Data.GAME_LOGIC.uno_Const import MODE_NORMAL, MODE_ALLCARD, MODE_CHANGECOLOR, MODE_OPENSHUFFLE, MODE_COMBO
 from Data.GAME_VIEW.OBJECT.button import Button
 from Data.GAME_VIEW.OBJECT.view import init_view
 from Data.GAME_VIEW.util import *
@@ -45,15 +46,22 @@ def story_mode(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
                           pos=(x_pos, y_pos + set_size(260, SCREEN_WIDTH)),
                           size=(BUTTON_WIDTH, BUTTON_HEIGHT))
 
+    if config['system']['STORY_B_WIN'] == "False":
+        story_mode_2.image = pygame.image.load(BUTTON_PATH + "story_mode_2_completed.png")
+    if config['system']['STORY_C_WIN'] == "False":
+        story_mode_3.image = pygame.image.load(BUTTON_PATH + "story_mode_3_completed.png")
+    if config['system']['STORY_D_WIN'] == "False":
+        story_mode_4.image = pygame.image.load(BUTTON_PATH + "story_mode_4_completed.png")
     init_view(SCREEN, [back_button, story_mode_1, story_mode_2, story_mode_3, story_mode_4, start_button])
 
     def show_popup(stage):
         global text, mode, text_rect
         font = pygame.font.Font(FONT_PATH, set_size(36, SCREEN_WIDTH))
         description = [
-            "첫 분배시 컴퓨터 플레이어가 기술 카드를 50% 더 높은 확률로 받게 됨.컴퓨터 플레이어가 거꾸로 진행과\n 건너 뛰기 등의 기술카드를 적절히 조합하여 2~3장 이상의 카드를 한 번에 낼 수 있는 콤보를 사용.",
+            "첫 분배시 컴퓨터 플레이어가 기술 카드를 50% 더 높은 확률로 받게 됨.",
             "3명의 컴퓨터 플레이어와 대전 / 첫 카드를 제외하고 모든 카드를 같은 수만큼 플레이어들에게 분배.",
             "2명의 컴퓨터 플레이어와 대전 / 매 5턴마다 낼 수 있는 카드의 색상이 무작위로 변경됨."
+            "5턴마다 무작위로 opencard가 셔플된다. 그 후 맨 위 카드의 효과가 적용됨."
         ]
         if stage == "A":
             text = font.render(description[0], True, (0, 0, 0))
@@ -72,16 +80,14 @@ def story_mode(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
                           text_rect.top - set_size(10, SCREEN_WIDTH),
                           text_rect.width + set_size(20, SCREEN_WIDTH),
                           text_rect.height + set_size(20, SCREEN_WIDTH)))
-
         pygame.draw.rect(SCREEN, (0, 0, 0),
                          (text_rect.left - set_size(10, SCREEN_WIDTH),
                           text_rect.top - set_size(10, SCREEN_WIDTH),
                           text_rect.width + set_size(20, SCREEN_WIDTH),
                           text_rect.height + set_size(20, SCREEN_WIDTH)), 2)
-
         SCREEN.blit(text, text_rect)
         pygame.display.flip()
-
+    popup = False
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -93,28 +99,31 @@ def story_mode(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
                 elif story_mode_1.rect.collidepoint(event.pos):
                     mode = "A"
                     show_popup(mode)
-                    from Data.GAME_VIEW.SCREEN.start import start_game
-                    start_game(int(config['system']['SCREEN_WIDTH']), int(config['system']['SCREEN_HEIGHT']), 2,
-                               ["USER", "COMPUTER1", "COMPUTER2"], color_weakness_value, MODE_COMBO)
-                elif story_mode_2.rect.collidepoint(event.pos) and config['system']['STORY_A_WIN'] == "True":
+                    popup = True
+                elif story_mode_2.rect.collidepoint(event.pos):
                     mode = "B"
                     show_popup(mode)
-                elif story_mode_3.rect.collidepoint(event.pos) and config['system']['STORY_B_WIN'] == "True":
+                    popup = True
+                elif story_mode_3.rect.collidepoint(event.pos):
                     mode = "C"
                     show_popup("C")
-                elif story_mode_4.rect.collidepoint(event.pos) and config['system']['STORY_C_WIN'] == "True":
-                    from Data.GAME_VIEW.SCREEN.loby import loby
-                    loby(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT, mode=MODE_OPENSHUFFLE)
-                elif text_rect.collidepoint(event.pos):
-                    init_bg(SCREEN, SCREEN_PATH + "story_mode_map.png", SCREEN_WIDTH, SCREEN_HEIGHT)
-                    init_view(SCREEN,
+                    popup = True
+                elif story_mode_4.rect.collidepoint(event.pos):
+                    mode = "D"
+                    show_popup("D")
+                    popup = True
+                elif popup:
+                    if text_rect.collidepoint(event.pos):
+                        init_bg(SCREEN, SCREEN_PATH + "story_mode_map.png", SCREEN_WIDTH, SCREEN_HEIGHT)
+                        init_view(SCREEN,
                               [back_button, story_mode_1, story_mode_2, story_mode_3, story_mode_4, start_button])
-                    pygame.display.flip()
+                        pygame.display.flip()
+                        popup = False
                 elif start_button.rect.collidepoint(event.pos):
                     if mode == "A":
                         from Data.GAME_VIEW.SCREEN.start import start_game
                         start_game(int(config['system']['SCREEN_WIDTH']), int(config['system']['SCREEN_HEIGHT']), 2,
-                                   ["USER", "COMPUTER1", "COMPUTER2"], color_weakness_value, MODE_ALLCARD)
+                                   ["USER", "COMPUTER1", "COMPUTER2"], color_weakness_value, MODE_COMBO)
                     elif mode == "B":
                         from Data.GAME_VIEW.SCREEN.start import start_game
                         start_game(int(config['system']['SCREEN_WIDTH']), int(config['system']['SCREEN_HEIGHT']), 2,
@@ -123,4 +132,7 @@ def story_mode(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT)
                         from Data.GAME_VIEW.SCREEN.start import start_game
                         start_game(int(config['system']['SCREEN_WIDTH']), int(config['system']['SCREEN_HEIGHT']), 2,
                                    ["USER", "COMPUTER1", "COMPUTER2"], color_weakness_value, MODE_CHANGECOLOR)
+                    elif mode == "D":
+                        from Data.GAME_VIEW.SCREEN.loby import loby
+                        loby(SCREEN, SCREEN_WIDTH, SCREEN_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT, MODE_OPENSHUFFLE)
         pygame.display.flip()
